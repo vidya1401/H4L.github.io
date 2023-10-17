77d050587ea60439a9e96705d03d1913a93e9ff3
vidya720 <hrithikboddam3@gmail.com>
2023-10-16 22:27:30 -0400

    ADDED

--- src/enviroments/enviroment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-
-  BEServer: {
-    DevEnviroment: 'https://localhost:7148/api/',
-    Server: '01234567890123456789012345678901',
-    UserServer: 'JtYl3o8okAWCxanUzbu8uw==',
-  },
-};
